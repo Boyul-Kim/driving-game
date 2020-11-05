ab680df2286c5a24acdca3668938fa7b8f7a83f0
Boyul Kim <boyulkim1@gmail.com>
2020-11-05 14:54:02 -0800

    made edits from change request - issue 2

--- main.js
+++ main.js
@@ -1,15 +1,27 @@
-var $body = document.querySelector('body');
 var $racecar = document.querySelector('.racecar');
+
+var car = {
+  direction: 'east'
+};
 
 function arrowKeys(event) {
   var direction = '';
+
   if (event.keyCode === 38) {
     direction = 'up';
+    car.direction = 'north';
   } else if (event.keyCode === 37) {
     direction = 'left';
+    car.direction = 'west';
   } else if (event.keyCode === 40) {
     direction = 'down';
+    car.direction = 'south';
+  } else if (event.keyCode === 39) {
+    direction = 'right';
+    car.direction = 'east';
   }
+
   $racecar.className = 'racecar rotate' + direction;
+
 }
-$body.addEventListener('keydown', arrowKeys);
+document.addEventListener('keydown', arrowKeys);
