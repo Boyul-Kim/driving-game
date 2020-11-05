b46c24ebafda95a2dee80e44e06d2c522adbf53d
Boyul Kim <boyulkim1@gmail.com>
2020-11-05 12:44:08 -0800

    created eventlistener for arrrow keys - issue 2

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
 var $body = document.querySelector('body');
-var $container = document.querySelector('.container');
-var $racecar = document.queryCommandEnabled('.racecar');
+// var $container = document.querySelector('.container');
+// var $racecar = document.queryCommandEnabled('.racecar');
+
+function arrowKeys(event) {
+  // keyCode left: 37
+  // keyCode right: 39
+  // keyCode up: 38
+  // keyCode down: 40
+  // console.log(event.keyCode);
+}
+$body.addEventListener('keydown', arrowKeys);
